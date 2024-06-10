03aa91ad761e945fcfdb71dc103f9e3862876dd0
TanHE176226 <you@example.com>
2024-06-10 20:33:06 +0700

    Update authen

--- API/middleware/authJWT.js
+++ API/middleware/authJWT.js
@@ -1,79 +1,51 @@
 import jwt from "jsonwebtoken";
 import User from "../models/users.js";
 
-// Check if user log in
-const requireAuth = (req, res, next) => {
-    const token = req.cookies.jwt;
-    //Check if jwt exist & is verified
-    if (token) {
-        jwt.verify(token, process.env.SIGNATURE, (err, decodedToken) => {
-            if (err) {
-                console.log(err.message);
-                res.redirect('/login');
-            } else {
-                console.log(decodedToken)
-                next();
-            }
-        });
-    }
-    else {
-        res.redirect('login');
-    }
-}
+function generateAccessToken(userID) {
+    return jwt.sign(userID, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10m' });
+};
 
-// Check if user is admin or not
-const isAdmin = (req, res, next) => {
-    const token = req.cookies.jwt;
-    if (token) {
-        jwt.verify(token, process.env.SIGNATURE, async (err, decodedToken) => {
-            if (err) {
-                console.log(err.message);
-                res.locals.user = null;
-                res.status(403).send({ message: "1" });
-            } else {
-                console.log(decodedToken);
-                // Get the ID of the user by jwt
-                const user = await User.findById(decodedToken.id);
-                console.log(decodedToken.id);
-                res.locals.user = user;
-                if (user.roleID != 0) {
-                    res.status(403).send({ message: "User must be an admin to access this api" });
-                }
-            }
-        });
-    }
-    else {
-        res.locals.user = null;
-        res.status(403).send({ message: "2" });
-    }
-}
-
-const isRecruiter = (req, res, next) => {
-    const token = req.cookies.jwt;
-
-    if (token) {
-        jwt.verify(token, process.env.SIGNATURE, async (err, decodedToken) => {
-            if (err) {
-                console.log(err.message);
-                res.locals.user = null;
-                next();
-            } else {
-                console.log(decodedToken);
-                // Get the ID of the user by jwt
-                let user = await User.findById(decodedToken.id);
-                res.locals.user = user;
-                if (user.roleID != 2 && user.roleID != 0) {
-                    res.status(403).send({ message: "User must be an recruiter to access this api" });
-                }
-                next();
-            }
-        });
-    }
-    else {
-        res.locals.user = null;
+function isAdmin(req, res, next) {
+    const user = req.user;
+    if (user.roleID === 0) {
         next();
+    } else {
+        res.sendStatus(403); // Access forbidden for non-admin users
     }
 }
+
+function isRecruiter(req, res, next) {
+    const user = req.user;
+    if (user.roleID === 2) {
+        next();
+    } else {
+        res.sendStatus(403); // Access forbidden for non-recruiter users
+    }
+}
+
+function authenticationToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    // If there is no token provided
+    if (token == null) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, decodedToken) => {
+        // If token is no longer valid
+        if (err) return res.sendStatus(403);
+
+        const userID = decodedToken.id;
+        try {
+            const user = await User.findById(userID);
+            req.user = user;
+
+            next();
+        } catch (error) {
+            console.error(error);
+            res.sendStatus(500);
+        }
+    });
+}
+
 
 export default {
     requireAuth,
