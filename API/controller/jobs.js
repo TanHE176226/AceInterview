89c5b34f81803562641f496a7621a327057cd659
TanHE176226 <134518939+TanHE176226@users.noreply.github.com>
2024-06-15 13:51:24 +0700

    Update jobs.js

--- API/controller/jobs.js
+++ API/controller/jobs.js
@@ -9,6 +9,43 @@ const getAllJobs = async (req, res) => {
     }
 }
 
+const getJobs = async (req, res) => {
+    try {
+        const { title, industry, typeOfWork, status, location, minSalary, maxSalary } = req.query;
+
+        const query = {};
+
+        if (title) {
+            query.title = new RegExp(title, 'i');
+        }
+
+        if (industry) {
+            query.industry = industry;
+        }
+        if (typeOfWork) {
+            query.typeOfWork = typeOfWork;
+        }
+        if (status) {
+            query.status = status;
+        }
+        if (location) {
+            query.location = location;
+        }
+        if (minSalary) {
+            query.salary = { ...query.salary, $gte: minSalary };
+        }
+        if (maxSalary) {
+            query.salary = { ...query.salary, $lte: maxSalary };
+        }
+
+        const jobs = await jobDAO.getJobs(query);
+        res.status(200).json(jobs);
+
+    } catch (error) {
+        res.status(500).json({ error: error.toString() });
+    }
+};
+
 const getJobDetails = async (req, res) => {
     try {
         const { jobId } = req.params;
