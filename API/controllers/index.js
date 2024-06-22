7c41e7ff4c78a83d67933d5f50b47721ef44284e
SVkdf092 <97460137+SomnvK123@users.noreply.github.com>
2024-06-22 18:12:19 +0700

    Delete API/controllers directory

--- API/controllers/index.js
+++
@@ -1,9 +0,0 @@
-import companiesController from "./companies.js";
-import jobController from "./jobs.js";
-import userController from "./users.js";
-import cvController from "./CVs.js";
-import jobAppliedController from "./jobApplieds.js";
-
-export {
-    companiesController, jobController, userController, cvController, jobAppliedController
-}
