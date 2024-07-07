14fec0b5c6fad5a4032b560f87ea1b957deae34d
SVkdf092 <97460137+SomnvK123@users.noreply.github.com>
2024-07-07 20:47:41 +0700

    Delete API directory

--- API/router/industries.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { industryController } from "../controller/index.js";
-
-const industryRouter = express.Router();
-
-// Get all industries
-industryRouter.get('/', industryController.getAllIndustries);
-
-export default industryRouter;
--- API/router/jobApplieds.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { jobAppliedController } from '../controller/index.js';
-
-const jobAppliedRouter = express.Router();
-
-jobAppliedRouter.get('/:applicantId', jobAppliedController.getAppliedJobs);
-jobAppliedRouter.post('/apply', jobAppliedController.applyForJob);
-
-export default jobAppliedRouter;
